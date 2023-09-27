026ac9bbd460febd93c8bee90923b6a13e554aea
mohammad-ali238 <123456847+mohammad-ali238@users.noreply.github.com>
2023-09-27 13:02:52 +0500

    Add files via upload

---
+++ csv-line.php
@@ -0,0 +1,130 @@
+<?php
+
+class csv_line implements \ArrayAccess, \Iterator
+{
+    /** @var string[] */
+    private $values;
+    /** @var string[] */
+    private $headers;
+    /** @var int */
+    private $position;
+
+    /**
+     * @param string[] $values
+     * @param string[] $headers
+     */
+    public function __construct(array $values = [], array $headers = [])
+    {
+        $this->values = $values;
+        $this->headers = $headers;
+        $this->position = 0;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getValues()
+    {
+        return $this->values;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param string|integer $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return !is_null($this->findValueIndex($offset));
+    }
+
+    /**
+     * @param string|integer $offset
+     * @return string|null
+     */
+    public function offsetGet($offset)
+    {
+        $index = $this->findValueIndex($offset);
+
+        return !is_null($index) && isset($this->values[$index]) ? $this->values[$index] : null;
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $index = $this->findValueIndex($offset);
+
+        if (!is_null($index) && isset($this->values[$index])) {
+            $this->values[$index] = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        $this->offsetSet($offset, null);
+    }
+
+    private function findValueIndex($offset)
+    {
+        $index = null;
+        if (in_array($offset, $this->headers)) {
+            $index = array_search($offset, $this->headers);
+        }
+        if (is_null($index)) {
+            $index = $offset;
+        }
+
+        return $index;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function current()
+    {
+        return $this->values[$this->position];
+    }
+
+    /**
+     *
+     */
+    public function next()
+    {
+        $this->position++;
+    }
+
+    /**
+     * @return string
+     */
+    public function key()
+    {
+        return isset($this->headers[$this->position]) ? $this->headers[$this->position] : $this->position;
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+        $countHeaders = count($this->headers);
+        if ($countHeaders > 0 && $this->position >= $countHeaders) {
+            return false;
+        }
+        return isset($this->values[$this->position]);
+    }
+
+    /**
+     *
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+}
---
+++ csv-reader.php
@@ -0,0 +1,155 @@
+<?php
+
+include 'csv-line.php';
+
+class csv_reader implements \Iterator
+{
+    /** @var resource */
+    private $handle;
+    /** @var string */
+    private $delimiter;
+    /** @var string */
+    private $enclosure;
+    /** @var string */
+    private $escape;
+    /** @var boolean */
+    private $parseHeaders;
+    /** @var bool */
+    private $clearBom;
+
+    /** @var string[] */
+    private $current;
+    /** @var integer */
+    private $position;
+    /** @var string[] */
+    private $headers;
+
+    /**
+     * @param resource $handle
+     * @param bool|string[] $headers
+     * @param string $delimiter
+     * @param string $enclosure
+     * @param string $escape
+     */
+    public function __construct($handle, $headers = true, $delimiter = ',', $enclosure = '"', $escape = '\\', $clearBom = false)
+    {
+        if (!is_resource($handle)) {
+            throw new \LogicException('Handler is not resource');
+        }
+        $this->handle = $handle;
+        $this->parseHeaders = $headers === true;
+        if (is_array($headers)) {
+            $this->headers = $headers;
+        } elseif ($headers === false) {
+            $this->headers = [];
+        }
+        $this->delimiter = $delimiter;
+        $this->enclosure = $enclosure;
+        $this->escape = $escape;
+        $this->clearBom = $clearBom;
+
+        $this->rewind();
+    }
+
+    /**
+     * @param array $headers
+     */
+    public function replaceHeaders(array $headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param $header
+     *
+     * @return bool
+     */
+    public function hasHeader($header)
+    {
+        return in_array($header, $this->headers);
+    }
+
+    /**
+     * @return csv_line
+     */
+    public function current()
+    {
+        return $this->current ? new csv_line($this->current, $this->headers) : false;
+    }
+
+    /**
+     *
+     */
+    public function next()
+    {
+        $this->current = $this->readLine();
+
+        if ($this->current !== false) {
+            $this->position++;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+        return $this->current !== false;
+    }
+
+    /**
+     *
+     */
+    public function rewind()
+    {
+        rewind($this->handle);
+
+        $this->position = -1;
+        if ($this->parseHeaders) {
+            $parsedHeaders = $this->readLine();
+            if (is_null($this->headers)) {
+                $this->headers = $parsedHeaders;
+            }
+        }
+
+        $this->next();
+    }
+
+    /**
+     * @return array|false|null
+     */
+    protected function readLine()
+    {
+        if ($this->clearBom && $this->position < 0) {
+            $line = fgets($this->handle);
+            $line = str_getcsv($this->removeBom($line), $this->delimiter, $this->enclosure, $this->escape);
+        } else {
+            $line = fgetcsv($this->handle, 0, $this->delimiter, $this->enclosure, $this->escape);
+        }
+
+        return $line;
+    }
+
+    private function removeBom($string)
+    {
+        $bom = pack('H*', 'EFBBBF');
+
+        return preg_replace('/^' . $bom . '/', '', $string);
+    }
+}
